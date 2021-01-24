72e5e77161350e59c438370d5043eb8b6ddf470b
jean0206 <jeanortiz00@gmail.com>
2021-01-23 22:29:06 -0500

    the footer was added

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,8 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
+      <v-btn icon href="https://github.com/jean0206/VacunaApp" target="blank">
+        <v-icon>mdi-github</v-icon>
       </v-btn>
 
       <v-btn icon>
@@ -23,6 +23,33 @@
     <v-main>
       <Home/>
     </v-main>
+     <v-footer
+     class="mt-6"
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="primary lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} <strong></strong>
+      </v-col>
+    </v-row>
+  </v-footer>
   </v-app>
 </template>
 

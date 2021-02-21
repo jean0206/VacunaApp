feadefb8af222c6ab4f5e17cb43c777445291893
jean0206 <jeanortiz00@gmail.com>
2021-02-21 02:34:29 -0500

    server modificado

--- src/lib/routeApi.js
+++ src/lib/routeApi.js
@@ -1,4 +1,4 @@
-const apiRoute = 'https://backendvacunapp.herokuapp.com/'
+const apiRoute = 'https://backendvacunapp.herokuapp.com'
 const apiTest = 'http://127.0.0.1:3030'
 
 module.exports={apiRoute}
